c7fcf702c4c78257166044614254a4e6e265d10e
forin.xyz <0000@whu.edu.cn>
2015-09-21 17:18:05 +0800

    first push

---
+++ datastructure_common.h
@@ -0,0 +1,23 @@
+#ifndef _DATASTRUCTURE_COMMON_H
+#define _DATASTRUCTURE_COMMON_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/*
+     宏
+*/
+#define compare(a, b) (a - b)
+#define ERROR -1 // the return value when error happens executing some functions
+#define SUCESS 0
+/*
+     别名
+*/
+typedef int object;
+#ifdef __cplusplus
+}
+#endif
+#endif /* datastructure_common.h */
